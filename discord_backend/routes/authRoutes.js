1f907f3ed289d2b6bf237fe56407a5cc0940e1c5
Nikita <nikitasharma2999@gmail.com>
2022-10-12 15:45:55 +0530

    register the routes

---
+++ discord_backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("register");
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send("login");
+});
