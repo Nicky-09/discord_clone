c7f528a0c1980bbedffe2c9d9761ba1ffc7a2191
Nikita <nikitasharma2999@gmail.com>
2022-10-13 11:26:09 +0530

    add register functionality

--- discord_backend/controllers/auth/postLogin.js
+++ discord_backend/controllers/auth/postLogin.js
@@ -1,5 +1,23 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const postLogin = async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+    const token = "1234567890";
+    res.status(200).json({ msg: "User logged in", email: email, token: token });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 module.exports = postLogin;
--- discord_backend/controllers/auth/postRegister.js
+++ discord_backend/controllers/auth/postRegister.js
@@ -1,5 +1,28 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+
 const postRegister = async (req, res) => {
-  res.send("register");
+  try {
+    const { email, password, username } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      email: email.toLowerCase(),
+      password: hash,
+      username,
+    });
+    await newUser.save();
+
+    const token = "1234567890";
+
+    res.status(200).json({ msg: "User created", email: email, token: token });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 module.exports = postRegister;
