7a534ce762914329d4aa69c98aad03e223312f19
Nikita <nikitasharma2999@gmail.com>
2022-10-13 10:49:38 +0530

    add login register controller and joi validation

--- discord_backend/routes/authRoutes.js
+++ discord_backend/routes/authRoutes.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const autControllers = require("../controllers/auth/authControllers");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("register");
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(20),
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.body);
-  res.send("login");
+const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(20),
+  username: Joi.string().required().min(4).max(12),
 });
+
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  autControllers.controllers.postRegister
+);
+
+router.get(
+  "/login",
+  validator.body(loginSchema),
+  autControllers.controllers.postLogin
+);
 
 module.exports = router;
