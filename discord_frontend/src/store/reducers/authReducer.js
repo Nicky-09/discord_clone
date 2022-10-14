b40186a9a07dafe3ff74e6ab82a0687279af4c02
Nikita <nikitasharma2999@gmail.com>
2022-10-14 08:32:40 +0530

    authReducer fix

--- discord_frontend/src/store/reducers/authReducer.js
+++ discord_frontend/src/store/reducers/authReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "authActions.SET_USER_DETAILS":
+    case authActions.SET_USER_DETAILS:
       return { ...state, userDetails: action.userDetails };
 
     default:
